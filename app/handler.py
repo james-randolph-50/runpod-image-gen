ff8eeb58824d8188330a84cfb8c408bf47b40264
James Randolph <james.randolph2016@gmail.com>
2025-02-13 11:50:36 -0500

    remove flux. print endpoints. simple input payload.

--- app/handler.py
+++ app/handler.py
@@ -7,31 +7,53 @@ load_dotenv()
 
 runpod.api_key = os.getenv("RUNPOD_API_KEY")
 
+endpoint = runpod.Endpoint("SERVER_ENDPOINT_ID")
 
-HUGGING_FACE_LOGIN = os.getenv("HUGGING_FACE_LOGIN")
+# Fetching all available endpoints
+endpoints = runpod.get_endpoints()
 
-print(torch.version.cuda)  # Should show CUDA version
-print(torch.cuda.device_count())  # Should be > 0.
+# Displaying the list of endpoints
+print(endpoints)
 
-from diffusers import FluxPipeline
-from huggingface_hub import login
+input_payload = {"input": {"prompt": "Hello, World! The runpod servless endpoint is working."}}
 
-# Authenticate with Hugging Face
-login(HUGGING_FACE_LOGIN)
+try:
+    endpoint = runpod.Endpoint("SERVER_ENDPOINT_ID")
+    run_request = endpoint.run(input_payload)
 
-print(torch.cuda.is_available())
+    # Initial check without blocking, useful for quick tasks
+    status = run_request.status()
+    print(f"Initial job status: {status}")
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
-#pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+    if status != "COMPLETED":
+        # Polling with timeout for long-running tasks
+        output = run_request.output(timeout=60)
+    else:
+        output = run_request.output()
+    print(f"Job output: {output}")
+except Exception as e:
+    print(f"An error occurred: {e}")
 
-prompt = "A puppy holding a sign that says hello world"
-image = pipe(
-    prompt,
-    height=1024,
-    width=1024,
-    guidance_scale=3.5,
-    num_inference_steps=50,
-    max_sequence_length=512,
-    generator=torch.Generator("cpu").manual_seed(0)
-).images[0]
-image.save("flux-dev.png")
+
+# HUGGING_FACE_LOGIN = os.getenv("HUGGING_FACE_LOGIN")
+
+# from diffusers import FluxPipeline
+# from huggingface_hub import login
+
+# # Authenticate with Hugging Face
+# login(HUGGING_FACE_LOGIN)
+
+# pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
+# #pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+
+# prompt = "A puppy holding a sign that says hello world"
+# image = pipe(
+#     prompt,
+#     height=1024,
+#     width=1024,
+#     guidance_scale=3.5,
+#     num_inference_steps=50,
+#     max_sequence_length=512,
+#     generator=torch.Generator("cpu").manual_seed(0)
+# ).images[0]
+# image.save("flux-dev.png")
